65cb990f2e700e889f7c2df07dd3a147cc6c8166
Ciaran <ccoady@tcd.ie>
2019-09-25 09:12:46 +0100

    Not matching multi line comments correctly

--- Assignment2/lex.yy.c
+++ Assignment2/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 6
-#define YY_END_OF_BUFFER 7
+#define YY_NUM_RULES 8
+#define YY_END_OF_BUFFER 9
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,10 +360,12 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[21] =
+static const flex_int16_t yy_accept[32] =
     {   0,
-        0,    0,    7,    5,    4,    5,    5,    5,    0,    0,
-        0,    0,    0,    0,    3,    2,    0,    1,    0,    0
+        0,    0,    9,    7,    6,    7,    7,    7,    7,    0,
+        4,    0,    0,    0,    0,    4,    3,    3,    0,    3,
+        0,    0,    0,    0,    5,    2,    0,    1,    0,    4,
+        0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -403,34 +405,42 @@ static const YY_CHAR yy_meta[8] =
         1,    2,    1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[24] =
+static const flex_int16_t yy_base[34] =
     {   0,
-        0,   26,   24,   31,   31,    5,   14,   14,    8,   15,
-       14,    8,   12,   18,   31,   31,   10,   31,   21,   31,
-       25,    8,   27
+        0,   50,   53,   35,   34,    7,   55,   14,   18,   33,
+       30,    0,   23,   27,   33,    0,   27,   55,   26,    0,
+       37,   43,   29,   28,   55,   55,   23,   55,    0,   20,
+       55,   22,   50
     } ;
 
-static const flex_int16_t yy_def[24] =
+static const flex_int16_t yy_def[34] =
     {   0,
-       20,    1,   20,   20,   20,   21,   22,   20,   21,   20,
-       21,   22,   22,   23,   20,   20,   23,   20,   23,    0,
-       20,   20,   20
+       31,    1,   31,   32,   32,   31,   31,   31,   32,   32,
+       32,    6,   33,   32,    6,    6,    8,   31,    8,    8,
+        8,   31,   31,   33,   31,   31,   22,   31,   22,   22,
+        0,   31,   31
     } ;
 
-static const flex_int16_t yy_nxt[39] =
+static const flex_int16_t yy_nxt[63] =
     {   0,
-        4,    5,    6,    4,    4,    7,    4,   10,   12,   11,
-       10,   18,   11,   16,   13,   20,   15,   14,   13,   18,
-       13,   19,   18,   20,   19,    9,    9,   17,   17,    8,
-        3,   20,   20,   20,   20,   20,   20,   20
+        4,    5,    6,    4,    7,    8,    4,   12,   13,   14,
+       12,   15,   12,   16,   17,   18,   17,   17,   19,   20,
+       21,   22,   10,   27,   11,   23,   27,   24,   25,   31,
+       25,   17,   17,   11,   31,   12,   11,   12,   26,   11,
+       11,   11,   17,   27,   28,   27,   29,   27,   27,   30,
+       13,   13,   31,    9,    3,   31,   31,   31,   31,   31,
+       31,   31
     } ;
 
-static const flex_int16_t yy_chk[39] =
+static const flex_int16_t yy_chk[63] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    6,   22,    6,
-        9,   17,    9,   13,   12,   11,   10,    8,   13,   14,
-        7,   14,   19,    3,   19,   21,   21,   23,   23,    2,
-       20,   20,   20,   20,   20,   20,   20,   20
+        1,    1,    1,    1,    1,    1,    1,    6,    6,    6,
+        6,    6,    6,    6,    8,    8,    8,    8,    8,    8,
+        8,    9,   32,   30,    9,   13,   27,   13,   14,   24,
+       23,   19,   17,   14,   15,   15,   11,   15,   21,   10,
+        5,    4,   21,   22,   22,   22,   22,   22,   22,   22,
+       33,   33,    3,    2,   31,   31,   31,   31,   31,   31,
+       31,   31
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -456,8 +466,8 @@ char *yytext;
 #line 7 "comments.l"
 
 
-#line 460 "lex.yy.c"
-#line 461 "lex.yy.c"
+#line 470 "lex.yy.c"
+#line 471 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -680,7 +690,7 @@ YY_DECL
 #line 11 "comments.l"
 
 
-#line 684 "lex.yy.c"
+#line 694 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -708,13 +718,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 21 )
+				if ( yy_current_state >= 32 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 31 );
+		while ( yy_base[yy_current_state] != 55 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -754,25 +764,37 @@ case 3:
 /* rule 3 can match eol */
 YY_RULE_SETUP
 #line 15 "comments.l"
-{printf("This is a string");}
+{printf("syntax error\n"); return 0;}
 	YY_BREAK
 case 4:
 /* rule 4 can match eol */
 YY_RULE_SETUP
 #line 16 "comments.l"
-{printf("matched a new line");}
+{printf("syntax error\n"); return 0;}
 	YY_BREAK
 case 5:
+/* rule 5 can match eol */
 YY_RULE_SETUP
 #line 17 "comments.l"
-{printf("matched random Character");}
+{printf("This is a string");}
 	YY_BREAK
 case 6:
+/* rule 6 can match eol */
+YY_RULE_SETUP
+#line 18 "comments.l"
+{printf("matched a new line");}
+	YY_BREAK
+case 7:
 YY_RULE_SETUP
 #line 19 "comments.l"
+{printf("matched random Character");}
+	YY_BREAK
+case 8:
+YY_RULE_SETUP
+#line 21 "comments.l"
 ECHO;
 	YY_BREAK
-#line 776 "lex.yy.c"
+#line 798 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1070,7 +1092,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 21 )
+			if ( yy_current_state >= 32 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1098,11 +1120,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 21 )
+		if ( yy_current_state >= 32 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 20);
+	yy_is_jam = (yy_current_state == 31);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1780,7 +1802,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 19 "comments.l"
+#line 21 "comments.l"
 
 
 int main()
